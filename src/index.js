a7e73ab158d37fa65b6222b05271d9c55e2cc1db
Gabriel OLiveira <gabrielerika8724@gmail.com>
2025-08-24 20:05:11 -0300

    adicionei isNan

--- src/index.js
+++ src/index.js
@@ -3,20 +3,26 @@ const readline = require('readline').createInterface({
     output: process.stdout,
 })
 
-readline.question('Digite sua primeira nota: ' , nota1 => {
-    readline.question('Digite sua segunda nota: ', nota2 => {
+readline.question('Digite sua primeira nota: ' , (nota1) => {
+    readline.question('Digite sua segunda nota: ', (nota2) => {
 
         let n1 = parseFloat(nota1);
         let n2 = parseFloat(nota2);
 
-        let media = (n1 + n2) / 2;
-
-        console.log(`Média: ${media}`);
+        if(isNaN(n1) || isNaN(n2)){
+            console.log('Erro: Você deve digitar números validos')
+        }else{
+            let media = (n1 + n2) /2;
+            console.log(`Média: ${media}`);
+        
 
         if(media >= 7 ){
             console.log('Aluno aprovado')
-        }else{console.log('Aluno reprovado')}
+        }else{console.log('Aluno reprovado')
+        }
+        }
 
         readline.close()
+    
     })
 })
