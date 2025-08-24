3846e44d9e2232e4e16d5e36cc6a08922c87c4ce
Gabriel OLiveira <gabrielerika8724@gmail.com>
2025-08-24 13:53:30 -0300

    Adiciona Readme.md com título calculaMedia

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+readline.question('Digite sua primeira nota: ' , nota1 => {
+    readline.question('Digite sua segunda nota: ', nota2 => {
+
+        let n1 = parseFloat(nota1);
+        let n2 = parseFloat(nota2);
+
+        let media = (n1 + n2) / 2;
+
+        console.log(`Média: ${media}`);
+
+        if(media >= 7 ){
+            console.log('Aluno aprovado')
+        }else{console.log('Aluno reprovado')}
+
+        readline.close()
+    })
+})
